ce3e261bd3f8a55b3bda93bf9a7410942630eeaa
blanc128 <blancbenjamin28@gmail.com>
2025-08-01 04:34:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~71Jgja6R#fpeXsf305wLJi7G-ITbhr3xrtxNImzSd2TQm_o1lRxo",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
-    MODE: process.env.MODE || "private",        // Bot mode: public/private/group/inbox
+    MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
     OWNER_NUMBER: process.env.OWNER_NUMBER || "50941964977",  // Owner's WhatsApp number
     
@@ -69,7 +69,7 @@ module.exports = {
 
     // ===== BOT BEHAVIOR & APPEARANCE =====
    
-    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",     
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "true",     
     // Allow public commands?
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
     // Show bot as always online?
